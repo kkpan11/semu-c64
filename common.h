578084280903ff0d1e227932efef3fffd97b5c7e
Onno Kortmann <onno@gmx.net>
2023-08-21 20:45:09 +0200

    C64: Fix ilog2(..) implementation

--- common.h
+++ common.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdint.h>
 #include "feature.h"
 
 #define unlikely(x) __builtin_expect((x), 0)
@@ -12,9 +13,17 @@
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(*(a)))
 
 /* Ensure that __builtin_clz is never called with 0 argument */
-static inline int ilog2(int x)
+static inline int ilog2(uint32_t x)
 {
+    // FIXME: likely inefficient (but also rarely used...)
+#if C64
+    uint8_t lz=0;
+    while(x>>=1)
+	lz++;
+    return lz;
+#else
     return 31 - __builtin_clz(x | 1);
+#endif
 }
 
 /* Range check
